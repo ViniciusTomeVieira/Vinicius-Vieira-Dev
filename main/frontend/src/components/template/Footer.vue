f1eaff0bae650c1b5db5b6a1024dfc4e7b45b023
Vinicius Tome Vieira <vinicius.vieira@delsoftsistemas.com.br>
2021-11-23 17:58:57 -0300

    chore: Create frontend template with vue

---
+++ main/frontend/src/components/template/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer">
+      <span>Copyright <strong>Vinícius Tomé Vieira</strong> © 2015-2018</span>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #DDD;
+        color: #333;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+</style>
