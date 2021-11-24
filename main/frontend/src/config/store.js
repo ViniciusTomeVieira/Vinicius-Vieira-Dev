54a017c5a21e2a2e8b4a7554b44e087cd6c5e3e1
Vinicius Tome Vieira <vinny.vieira99@hotmail.com>
2021-11-24 17:21:56 -0300

    feat: Create the frontend face with templates

---
+++ main/frontend/src/config/store.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        isMenuVisible: true,
+        user: {
+            name: 'Usuário Mock',
+            email: 'mock@cod3r.com.br'
+        }
+    },
+    mutations: {
+        toggleMenu(state, isVisible) {
+            if (isVisible === undefined){
+                state.isMenuVisible = !state.isMenuVisible
+            } else {
+                state.isMenuVisible = isVisible
+            }
+        }
+    }
+})
--- main/frontend/src/main.js
+++ main/frontend/src/main.js
@@ -1,9 +1,16 @@
+import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 
 import App from './App'
 
+import './config/bootstrap'
+import store from './config/store'
+import router from './config/router'
+
 Vue.config.productionTip = false
 
 new Vue({
+  store,
+  router,
   render: h => h(App)
 }).$mount('#app')
